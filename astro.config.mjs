6bed657891ace6aaa632159c2ec3891b810ae88b
Xiaochao Liu <xiaochao.liu@compass.com>
2025-12-22 10:05:05 +0800

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "https://s5blog.s5.workers.dev",
     integrations: [mdx(), sitemap(), db()],
     adapter: cloudflare({
         platformProxy: {

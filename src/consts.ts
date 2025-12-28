dd556d8fbd50cfc0de57d1f2465fea57c1a7744a
Xiaochao Liu <xiaochao.liu@compass.com>
2025-12-28 17:54:55 +0800

    UI

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,5 @@
 export const SITE_TITLE = "[米随随] s5s5";
 export const SITE_DESCRIPTION =
   "本站是基于人类交互的，您的想象决定了我们的应用！";
+export const BEI_AN = "蜀ICP备16014041号-1";
 export const CDN_URL = "https://pub-bb954768211c43c8b9f7029411c7b953.r2.dev";

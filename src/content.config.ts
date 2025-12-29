11ffd94a31a8611eeb4eedeb566a57e867cc772b
Xiaochao Liu <xiaochao.liu@compass.com>
2025-12-29 18:39:00 +0800

    weather

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 import { TZDate } from "@date-fns/tz";
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
+import { TIMEZONE } from "./consts.ts";
 
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
@@ -10,14 +11,15 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string().optional(),
     // 强制将不带时区的字符串解析为 Asia/Shanghai 时间
-    pubDate: z.string().transform((val) => new TZDate(val, "Asia/Shanghai")),
+    pubDate: z.string().transform((val) => new TZDate(val, TIMEZONE)),
     updatedDate: z
       .string()
       .optional()
-      .transform((val) => (val ? new TZDate(val, "Asia/Shanghai") : undefined)),
-    heroImage: z.string().optional(),
+      .transform((val) => (val ? new TZDate(val, TIMEZONE) : undefined)),
     category: z.string().optional(),
-    weather: z.string().optional()
+    location: z.string().optional(),
+    weather: z.string().optional(),
+    heroImage: z.string().optional()
   })
 });
 
